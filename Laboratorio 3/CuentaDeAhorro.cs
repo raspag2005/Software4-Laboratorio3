a5bc32044be226c0944a647b369391d28641e496
FISC <fhad@fahsdh.com>
2025-10-02 10:20:19 -0500

    Resolución del Problema 1

---
+++ Laboratorio 3/CuentaDeAhorro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3
+{
+    internal class CuentaDeAhorro
+    {
+        private int saldo;
+        private string nom;
+
+        public void setSaldo(int saldo)
+        {
+            this.saldo = saldo;
+        }
+        public int getSaldo()
+        {
+            return this.saldo;
+        }
+        public void deposito(int monto)
+        {
+            saldo += monto;
+        }
+        public void retiro(int monto)
+        {
+            saldo -= monto;
+        }
+        public bool puedeEliminar(int monto)
+        {
+            int saldoTeorico = saldo;
+            return (saldoTeorico - monto) > -1;
+        }
+        public string getNom()
+        {
+            return nom;
+        }
+        public void setNom(string nom) { this.nom = nom; }
+    }
+}
--- Laboratorio 3/Problema 1.cs
+++ Laboratorio 3/Problema 1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,122 @@ namespace Laboratorio_3
         public Problema1()
         {
             InitializeComponent();
+        }
+        CuentaDeAhorro clase = new CuentaDeAhorro();
+
+        public void setSaldoActual()
+        {
+            txtSaldoActual.Text = clase.getSaldo().ToString();
+        }
+        private void btnAbrirCuenta_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtMontoInicial.Text, out int montoInicial))
+            {
+                if (montoInicial > 0)
+                {
+                    if (! String.IsNullOrEmpty(txtNom.Text))
+                    {
+                        lblError1.Visible = false;
+                        btnAbrirCuenta.Enabled = false;
+                        txtNom.Enabled = false;
+                        txtMontoInicial.Enabled = false;
+                        rdbDepositos.Enabled = true;
+                        rdbRetiros.Enabled = true;
+                        txtMonto.Enabled = true;
+                        btnNuevo.Enabled = true;
+                        clase.setSaldo(montoInicial);
+                        clase.setNom(txtNom.Text);
+                        setSaldoActual();
+                    } else
+                    {
+                        lblError1.Text = "El Cliente debe tener nombre!";
+                        lblError1.Visible = true;
+                    }
+                } else
+                {
+                    lblError1.Text = "Monto Inicial debe ser un monto positivo!";
+                    lblError1.Visible = true;
+                }
+            } else
+            {
+                lblError1.Text = "Monto Inicial debe ser un número!";
+                lblError1.Visible = true;
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtMonto.Text, out int monto) && monto > 0)
+            {
+                lblError4.Visible = false;
+                lblError3.Visible = false;
+                lblError2.Visible = false;
+                if (rdbRetiros.Checked)
+                {
+                    if(monto <= clase.getSaldo())
+                    {
+                        lstRetiros.Items.Add(txtMonto.Text);
+                        clase.retiro(monto);
+                        setSaldoActual();
+                    } else
+                    {
+                        lblError2.Text = "Monto retirado no puede ser mayor que tu Saldo Actual!";
+                        lblError2.Visible = true;
+                    }
+                }
+                else if (rdbDepositos.Checked)
+                {
+                    lstDepositos.Items.Add(txtMonto.Text);
+                    clase.deposito(monto);
+                    setSaldoActual();
+                }
+                else {
+                    lblError3.Text = "No puedes hacer una nueva transacción sin elegir que tipo será!";
+                    lblError3.Visible = true; 
+                }
+            }
+            else
+            {
+                lblError4.Visible = false;
+                lblError3.Visible = false;
+                lblError2.Text = "El Monto puesto debe ser un número válido!";
+                lblError2.Visible = true;
+            }
+        }
+        private void lstRetiros_DoubleClick(object sender, EventArgs e)
+        {
+            Console.WriteLine(lstRetiros.SelectedItem.ToString());
+            if (int.TryParse(lstRetiros.SelectedItem.ToString(), out int monto))
+            {
+                lblError3.Visible = false;
+                lblError2.Visible = false;
+                    lblError4.Visible = false;
+                    lstRetiros.Items.RemoveAt(lstRetiros.SelectedIndex);
+                    clase.deposito(monto);
+                    setSaldoActual();
+            }
+            else MessageBox.Show("Yo no sé que hiciestes. Pero es un error", "ERROR..??", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void lstDepositos_DoubleClick(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(lstDepositos.SelectedItem.ToString(), out int monto))
+            {
+                lblError3.Visible = false;
+                lblError2.Visible = false;
+                if (clase.puedeEliminar(monto))
+                {
+                    lblError4.Visible = false;
+                    lstDepositos.Items.Remove(lstDepositos.SelectedItem);
+                    clase.retiro(monto);
+                    setSaldoActual();
+                }
+                else
+                {
+                    lblError4.Visible = true;
+                }
+            }
+            else MessageBox.Show("Yo no sé que hiciestes. Pero es un error", "ERROR..??", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
