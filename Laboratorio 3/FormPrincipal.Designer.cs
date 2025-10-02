31970bb663c8344a04304849da74f00396334785
FISC <fhad@fahsdh.com>
2025-10-02 08:38:42 -0500

    MenuStrip muestra otros Forms correctamente

--- Laboratorio 3/FormPrincipal.Designer.cs
+++ Laboratorio 3/FormPrincipal.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.components = new System.ComponentModel.Container();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.ahorrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridViewDeNumsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.mnuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.Problema1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Problema2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Problema3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Problema4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPrincipal.SuspendLayout();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -43,55 +43,62 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // menuStrip1
+            // mnuPrincipal
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ahorrosToolStripMenuItem,
-            this.dataGridViewDeNumsToolStripMenuItem,
-            this.toolStripMenuItem1,
-            this.toolStripMenuItem2});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
+            this.mnuPrincipal.BackColor = System.Drawing.SystemColors.Control;
+            this.mnuPrincipal.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mnuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Problema1,
+            this.Problema2,
+            this.Problema3,
+            this.Problema4});
+            this.mnuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.mnuPrincipal.Name = "mnuPrincipal";
+            this.mnuPrincipal.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.mnuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.mnuPrincipal.TabIndex = 1;
+            this.mnuPrincipal.Text = "menuStrip1";
             // 
-            // ahorrosToolStripMenuItem
+            // Problema1
             // 
-            this.ahorrosToolStripMenuItem.Name = "ahorrosToolStripMenuItem";
-            this.ahorrosToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
-            this.ahorrosToolStripMenuItem.Text = "1. Ahorros";
+            this.Problema1.Name = "Problema1";
+            this.Problema1.Size = new System.Drawing.Size(73, 20);
+            this.Problema1.Text = "1. Ahorros";
+            this.Problema1.Click += new System.EventHandler(this.Problema1_Click);
             // 
-            // dataGridViewDeNumsToolStripMenuItem
+            // Problema2
             // 
-            this.dataGridViewDeNumsToolStripMenuItem.Name = "dataGridViewDeNumsToolStripMenuItem";
-            this.dataGridViewDeNumsToolStripMenuItem.Size = new System.Drawing.Size(153, 20);
-            this.dataGridViewDeNumsToolStripMenuItem.Text = "2. DataGridView de Nums";
-            this.dataGridViewDeNumsToolStripMenuItem.Click += new System.EventHandler(this.dataGridViewDeNumsToolStripMenuItem_Click);
+            this.Problema2.Name = "Problema2";
+            this.Problema2.Size = new System.Drawing.Size(153, 20);
+            this.Problema2.Text = "2. DataGridView de Nums";
+            this.Problema2.Click += new System.EventHandler(this.Problema2_Click);
             // 
-            // toolStripMenuItem1
+            // Problema3
             // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(78, 20);
-            this.toolStripMenuItem1.Text = "3. Importes";
+            this.Problema3.Name = "Problema3";
+            this.Problema3.Size = new System.Drawing.Size(78, 20);
+            this.Problema3.Text = "3. Importes";
+            this.Problema3.Click += new System.EventHandler(this.Problema3_Click);
             // 
-            // toolStripMenuItem2
+            // Problema4
             // 
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(71, 20);
-            this.toolStripMenuItem2.Text = "4. Usuario";
+            this.Problema4.Name = "Problema4";
+            this.Problema4.Size = new System.Drawing.Size(71, 20);
+            this.Problema4.Text = "4. Usuario";
+            this.Problema4.Click += new System.EventHandler(this.Problema4_Click);
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.mnuPrincipal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnuPrincipal;
             this.Name = "FormPrincipal";
             this.Text = "Lab 3";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.mnuPrincipal.ResumeLayout(false);
+            this.mnuPrincipal.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,11 +107,11 @@
         #endregion
 
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem ahorrosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem dataGridViewDeNumsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.MenuStrip mnuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem Problema1;
+        private System.Windows.Forms.ToolStripMenuItem Problema2;
+        private System.Windows.Forms.ToolStripMenuItem Problema3;
+        private System.Windows.Forms.ToolStripMenuItem Problema4;
     }
 }
 
--- Laboratorio 3/FormPrincipal.cs
+++ Laboratorio 3/FormPrincipal.cs
@@ -17,9 +17,36 @@ namespace Laboratorio_3
             InitializeComponent();
         }
 
-        private void dataGridViewDeNumsToolStripMenuItem_Click(object sender, EventArgs e)
+        private void Problema1_Click(object sender, EventArgs e)
         {
+            Problema1 prob1 = new Problema1();
+            prob1.MdiParent = this;
+            prob1.WindowState = FormWindowState.Maximized;
+            prob1.Show();
+        }
 
+        private void Problema2_Click(object sender, EventArgs e)
+        {
+            Problema2 prob2 = new Problema2();
+            prob2.MdiParent = this;
+            prob2.WindowState = FormWindowState.Maximized;
+            prob2.Show();
+        }
+
+        private void Problema3_Click(object sender, EventArgs e)
+        {
+            Problema3 prob3 = new Problema3();
+            prob3.MdiParent = this;
+            prob3.WindowState = FormWindowState.Maximized;
+            prob3.Show();
+        }
+
+        private void Problema4_Click(object sender, EventArgs e)
+        {
+            Problema4 prob4 = new Problema4();
+            prob4.MdiParent = this;
+            prob4.WindowState = FormWindowState.Maximized;
+            prob4.Show();
         }
     }
 }
